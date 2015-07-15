85877861d53ed2ae8d030ccd0ca9a10d1e731e2b
AddisonT <addisontam1@Addisons-MacBook-Pro.local>
2015-07-15 15:44:34 -0700

    added controllers and seed of 1 box

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Box.create(name: "Classic Whiskey Smash",
+	image: "https://slack-files.com/files-tmb/T07F8G7N2-F07L32HMF-e92d24ca25/screenshot_2015-07-13_at_12.50.17_pm_1024.png",
+	description: "Speak nice and easy with this timeless-yet-daring concoction of rye whiskey, artisanal simple syrup, organic lemons, and fresh mint sprigs.  No passwords necessary here.",
+	ingredients: "rye, lemon, simple syrup, mint",
+	price: 60,
+	drink_count: 12)
